c68be8b87ffedb8fb3f28fcb97af8c3e50165d23
hyunwoopark <143897140+idiotist1@users.noreply.github.com>
2026-05-11 01:38:08 +0900

    One스크립트 수정
    One스크립트 수정

--- Assets/One.cs
+++ Assets/One.cs
@@ -2,9 +2,8 @@ using UnityEngine;
 
 public class One : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public int Damage;
 
-    // Update is called once per frame
     void Update()
     {
         
