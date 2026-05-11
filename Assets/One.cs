d7c03c946f63a9ff72b3c4d65fd8e14cd64c7ddf
hyunwoopark <143897140+idiotist1@users.noreply.github.com>
2026-05-12 05:36:21 +0900

    Develop 수정
    Develop 수정

--- Assets/One.cs
+++ Assets/One.cs
@@ -3,9 +3,15 @@ using UnityEngine;
 public class One : MonoBehaviour
 {
     public int Damage;
+    public float Speed;
 
     void Start()
     {
-        Debug.Log("Develop!!");
+        Debug.Log("Ãæµ¹!");
+    }
+
+    void Update()
+    {
+
     }
 }
